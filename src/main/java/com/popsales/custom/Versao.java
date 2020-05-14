0058f58a93e19b8bd22b7458f0620271263dfd93
Renato <contato.renatosi@hotmail.com>
2020-05-14 10:23:17 -0300

    Merge origin/master
    Conflicts:
    	src/main/webapp/fecharPedido.xhtml

---
+++ src/main/java/com/popsales/custom/Versao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.popsales.custom;
+
+/**
+ *
+ * @author Renato
+ */
+public class Versao {
+
+    private Integer versao;
+
+    public Integer getVersao() {
+        return versao;
+    }
+
+    public void setVersao(Integer versao) {
+        this.versao = versao;
+    }
+
+}
